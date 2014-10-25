36c982fbe9203363ab81a80f757872aa05600fd8
DasWookie <github@midnightfantasy.com>
2014-10-25 04:28:52 -0500

    modified:   Sword/Sword.ino 	Updates for impact detection

--- Sword/Sword.ino
+++ Sword/Sword.ino
@@ -1,17 +1,17 @@
 /* WOW Sword sketch to create specular pattern
- * you should see a pattern on the LED strip that fades from
- * green to pink and also moves along the strip.
+ * You should see a pattern of various shades and brightness of green...
+ * until a hit or impact is detected, at which point all LEDs go RED for a short duration
 */
 
 #include <PololuLedStrip.h>
 
-// Create an ledStrip object and specify the pin it will use.
+// Create ledStrip objects and specify pins used
 PololuLedStrip<11> ledStrip1;
 PololuLedStrip<12> ledStrip2;
 
 // Create READ pin for impact sensor
-#define hitSensor  A0
-#define modeSwitch 10
+#define hitSensor  A0                // Using an analog pin would allow for either a vibration (piezo) or mechanical switch to be used. Using a piezo would require filtering for threshold minimum levels.  
+#define modeSwitch 10                // Use a SPST momentary contact switch
 
 // Create a buffer for holding the colors (3 bytes per color).
 #define LED_COUNT  55
@@ -23,6 +23,7 @@ bool               hitSensorReading;
 bool               modeSwitchReading;
 int                hitDelay;
 int                pass;
+int                passUpdate;
 int                currentMode;
 int                totalModes;
 int                minBrightness;
@@ -36,6 +37,7 @@ void setup()
   minBrightness   = 25;              // Lowest level of illumination  (0 is off recommend not going under 25)
   maxBrightness   = 255;             // Maximum level of illumination (255 max)
   hitDelay        = 150;             // Amount of time in milliseconds to show RED after an impact/hit is detected
+  passUpdate      = 20;              // Number of loops before we update the LED colors. In general each pass should take about 1ms, but that MAY differ based on the frequency of your crystal and the specific microcontroller used
 
   // Globals
   pinMode(hitSensor, INPUT);
@@ -47,9 +49,9 @@ void setup()
 
 void loop()
 {
-  pass++;        // Increment pass count, we use this rather than delays at each loop so we keep the loops tight for hit detection
-  checkHit();    // check the impact sensor
-  if (pass >= 10) {
+  pass++;                  // Increment pass count, we use this rather than large delays at each loop so we can keep the loops tight for hit detection
+  checkHit();              // Let the bodies hit the floor!
+  if (pass >= passUpdate) {
     // Update the colors.
     byte time = millis() >> 2;
     for(uint16_t i = 0; i < LED_COUNT; i++)
@@ -63,8 +65,6 @@ void loop()
       colors2[i] = (rgb_color){ 
         0, random(setBrightness), 0     };
     }
-
-    // Write the colors to the LED strip.
     ledStrip1.write(colors1, LED_COUNT);  
     ledStrip2.write(colors2, LED_COUNT);
     pass = 0;
@@ -94,12 +94,10 @@ void checkModeSwitch() {
       for(uint16_t i = 0; i < LED_COUNT; i++)
       {
         colors1[i] = (rgb_color){
-          random(setBrightness), random(setBrightness), random(setBrightness) };
+          random(setBrightness), random(setBrightness), random(setBrightness) };      // Taste the Rainbow!
       }
-      // Write the colors to the LED strip.
       ledStrip1.write(colors1, LED_COUNT);  
-      ledStrip2.write(colors1, LED_COUNT);
-      
+      ledStrip2.write(colors1, LED_COUNT);      
       delay(100);
 
       for(uint16_t i = 0; i < LED_COUNT; i++)
@@ -107,27 +105,23 @@ void checkModeSwitch() {
         colors1[i] = (rgb_color){ 
           0, 0, 0 };
       }
-  
-      // Write the colors to the LED strip.
       ledStrip1.write(colors1, LED_COUNT);  
       ledStrip2.write(colors1, LED_COUNT);
       delay(100);
     }
 
-    delay(hitDelay * 3);                              // crude debounce just to make sure we don't skip over modes too quickly.
-      
+    delay(hitDelay * 3);                              // crude debounce just to make sure we don't skip over modes too quickly.      
   }
 }
 
 void checkHit() {
   hitSensorReading = digitalRead(hitSensor);    
-  if (hitSensorReading == HIGH) {
+  if (hitSensorReading == HIGH) {                     // We've got an impact, show the BLOOD!
     for(uint16_t i = 0; i < LED_COUNT; i++) {
       colors1[i] = (rgb_color){ 
-        random(setBrightness), 0, 0};
+        random(maxBrightness), 0, 0};                 // Deliberate override of users set current mode. Using MAX brightness rather than SET, to be RED, which if set dim could be rather... pink.
     }
-    // Write the colors to the LED strip.
-    ledStrip1.write(colors1, LED_COUNT);  
+    ledStrip1.write(colors1, LED_COUNT);
     ledStrip2.write(colors1, LED_COUNT);
     delay(hitDelay);
   }
