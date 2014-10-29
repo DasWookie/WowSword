327c567cf078e8ca7d1ec3b87ee0ac45aa438e66
DasWookie <github@midnightfantasy.com>
2014-10-28 22:15:46 -0500

    modified:   Sword.ino 	reorganize variables, add more comments, implement min brightness

--- Sword/Sword.ino
+++ Sword/Sword.ino
@@ -5,14 +5,12 @@
 
 #include <PololuLedStrip.h>
 
-// Create ledStrip objects and specify pins used
-PololuLedStrip<11> ledStrip1;
-PololuLedStrip<12> ledStrip2;
-
-// Create READ pin for impact sensor
-#define hitSensor  A0                // Using an analog pin would allow for either a vibration (piezo) or mechanical switch to be used. Using a piezo would require filtering for threshold minimum levels.  
-#define modeSwitch 10                // Use a SPST momentary contact switch
-#define heartbeat  13
+// Pin assignments
+const byte         hitSensor  = A0;          // Using an analog pin would allow for either a vibration (piezo) or mechanical switch to be used. Using a piezo would require filtering for threshold minimum levels.  
+const byte         modeSwitch = 10;          // Use a SPST momentary contact switch
+PololuLedStrip<11> ledStrip1;                // Create ledStrip objects and specify pins used
+PololuLedStrip<12> ledStrip2;                // Create ledStrip objects and specify pins used
+const byte         heartbeat  = 13;          // Pin to use to track sword heartbeat, pin 13 uses the onboard LED
 
 // Create a buffer for holding the colors (3 bytes per color).
 #define LED_COUNT  55
@@ -20,50 +18,45 @@ rgb_color          colors1[LED_COUNT];
 rgb_color          colors2[LED_COUNT];
 
 // State Variables
-bool               hitSensorReading;
-bool               modeSwitchReading;
-bool               heart;
-int                hitDelay;
-int                pass;
-int                passUpdate;
-int                currentMode;
-int                totalModes;
-int                minBrightness;
-int                maxBrightness;
-int                setBrightness;
-int                stepBrightness;
+bool               hitSensorReading;         // Value to determine if an impact has been detected
+bool               modeSwitchReading;        // Value to determine if the mode switch button is being pressed
+bool               heart             = true; // Value to track if heartbeat LED is enabled or disabled on current loop
+int                currentMode;              // Tracker for illumination level mode
+int                pass;                     // Tracker for loop iteration
+int                setBrightness;            // Currently set brightness level
+int                stepBrightness;           // Calculated value for max brightness / total modes
+
+// User Define Variables to configure sword
+int                hitDelay          = 250;  // Amount of time in milliseconds to show RED after an impact/hit is detected
+int                passUpdate        = 100;  // Number of loops before we update the LED colors. In general each pass should take about 1ms, but that MAY differ based on the frequency of your crystal and the specific microcontroller used
+int                totalModes        = 5;    // Total number of dimness levels / too high of granularity will make it difficult to tell which mode you are in but greater granularity of battery life
+int                minBrightness     = 25;   // Lowest level of illumination  (0 is off recommend not going under 25)
+int                maxBrightness     = 255;  // Maximum level of illumination (255 max)
 
 void setup()
 {
-  totalModes      = 5;               // Total number of dimness levels / too high of granularity will make it difficult to tell which mode you are in but greater granularity of battery life
-  minBrightness   = 25;              // Lowest level of illumination  (0 is off recommend not going under 25)
-  maxBrightness   = 255;             // Maximum level of illumination (255 max)
-  hitDelay        = 250;             // Amount of time in milliseconds to show RED after an impact/hit is detected
-  passUpdate      = 100;              // Number of loops before we update the LED colors. In general each pass should take about 1ms, but that MAY differ based on the frequency of your crystal and the specific microcontroller used
-
   // Globals
   pinMode(hitSensor, INPUT);
   pinMode(modeSwitch, INPUT);
   pinMode(heartbeat, OUTPUT); 
   stepBrightness  = (maxBrightness / totalModes);
-  pass            = 0;
-  heart           = true;
-  currentMode     = passUpdate;      // Set to update on first loop so we don't have to wait for initial light illumination
-  
-  startupTest();                     // Perform startup test to illuminate all LEDs sequentially to ensure they all work
+  pass            = passUpdate;       // Set to update on first loop so we don't have to wait for initial light illumination
+  currentMode     = totalModes;       // Set initial light illumination level to brightest level (0 = dimmest, total modes = brightest)
+
+  startupTest();                      // Perform startup test to illuminate all LEDs sequentially to ensure they all work
 }
 
 void loop()
 {
-  pass++;                  // Increment pass count, we use this rather than large delays at each loop so we can keep the loops tight for hit detection
-  checkHit();              // Let the bodies hit the floor!
+  pass++;                             // Increment pass count, we use this rather than large delays at each loop so we can keep the loops tight for hit detection
+  checkHit();                         // Let the bodies hit the floor!
   if (pass >= passUpdate) {
     green();
     digitalWrite(heartbeat, heart);   // set the heartbeat LED state
     heart = !heart;                   // toggle the status on/off;
     pass = 0;
   } else {
-    checkModeSwitch();        // See if our mode has changed
+    checkModeSwitch();                // See if our mode has changed
     delay(1);
   }
 }
@@ -83,9 +76,7 @@ void checkModeSwitch() {
     if (setBrightness <= minBrightness) {
       setBrightness = minBrightness;
     }
-
-    // On Mode change, blink the sword currentMode+1 number of times to show which mode you are currently set to
-    blink(currentMode+1);
+    blink(currentMode+1);                             // On Mode change, blink the sword currentMode+1 (it's base 0) number of times to show which mode you are currently set to
     delay(hitDelay * 3);                              // crude debounce just to make sure we don't skip over modes too quickly.      
   }
 }
@@ -94,7 +85,6 @@ void blink(int count) {
     for(int i = 1; i <= count; i++) {
       rainbow();
       delay(250);
-
       off();
       delay(100);
     }
@@ -112,31 +102,41 @@ void green() {
     for(uint16_t i = 0; i < LED_COUNT; i++)
     {
       colors1[i] = (rgb_color){ 
-        0, random(setBrightness), 0     };
+        0, illuminationLevel(setBrightness), 0};
       colors2[i] = (rgb_color){
-        0, random(setBrightness), 0     };
+        0, illuminationLevel(setBrightness), 0};
     }
     ledStrip1.write(colors1, LED_COUNT);  
     ledStrip2.write(colors2, LED_COUNT);
 }
 
-void red() {
+void red() {                                        // Set the blade to RED to show the "blood" of an impact
     for(uint16_t i = 0; i < LED_COUNT; i++) {
-      colors1[i] = (rgb_color){ 
-        random(maxBrightness), 0, 0};                 // Deliberate override of users set current mode. Using MAX brightness rather than SET, to be RED, which if set dim could be rather... pink.
+      colors1[i] = (rgb_color){
+        illuminationLevel(maxBrightness), 0, 0};    // Deliberate override of users set current mode. Using MAX brightness rather than SET, to be RED, which if set dim could be rather... pink.
+      colors2[i] = (rgb_color){
+        illuminationLevel(maxBrightness), 0, 0};
     }
     ledStrip1.write(colors1, LED_COUNT);
-    ledStrip2.write(colors1, LED_COUNT);
+    ledStrip2.write(colors2, LED_COUNT);
 }
 
-void rainbow() {
-        for(uint16_t i = 0; i < LED_COUNT; i++)
-      {
-        colors1[i] = (rgb_color){
-          random(setBrightness), random(setBrightness), random(setBrightness) };      // Taste the Rainbow!
-      }
-      ledStrip1.write(colors1, LED_COUNT);  
-      ledStrip2.write(colors1, LED_COUNT);      
+void rainbow() {                                    // Taste the Rainbow!
+  for(uint16_t i = 0; i < LED_COUNT; i++)
+    {
+      colors1[i] = (rgb_color){
+      random(setBrightness), random(setBrightness), random(setBrightness) };
+    }
+    ledStrip1.write(colors1, LED_COUNT);  
+    ledStrip2.write(colors1, LED_COUNT);      
+}
+
+int  illuminationLevel(int illumination) {          // Return a INT between minimum brightness level and max brightness level
+  illumination = random(illumination);
+  if (illumination < minBrightness) {
+    illumination = minBrightness;
+  }
+  return (illumination);
 }
 
 void off() {
@@ -151,7 +151,7 @@ void off() {
 
 void startupTest() {
   boolean side = true;                                // Bool to track which side is currently displaying gradient and which is showing rainbow
-  for(int testLoop = LED_COUNT; testLoop > LED_COUNT; testLoop++) {
+  for(int testLoop = 0; testLoop <= 10; testLoop++) {
     byte time = millis() >> 2;
     for(uint16_t i = 0; i < LED_COUNT; i++)
     {
@@ -167,8 +167,8 @@ void startupTest() {
     } else {
       ledStrip2.write(colors1, LED_COUNT);  
       ledStrip1.write(colors2, LED_COUNT);  
-    }      
+    }
     side = !side;                                     // Flip the sides the colors are being written to so each side gets both gradient as well as random rainbow tests
-    delay(50);                                        // Delay between cycling to next LED
+    delay(hitDelay);                                  // Delay between cycling to next LED
   }
 }
